658b7a165ba7df306dd74d4603cb12b7015db688
andra <andrasurya.100@gmail.com>
2023-03-01 11:49:11 +0700

    modal fade

--- resources/views/admin/tbsem/pengetahuan/nilaip.blade.php
+++ resources/views/admin/tbsem/pengetahuan/nilaip.blade.php
@@ -168,7 +168,7 @@
 
 
 <!-- Modal --->
-<div class="modal" id="Input" tabindex="-1" role="dialog" aria-labelledby="InputLabel" aria-hidden="true">
+<div class="modal fade" id="Input" tabindex="-1" role="dialog" aria-labelledby="InputLabel" aria-hidden="true">
   <div class="modal-dialog modal-xl" role="document">
     <div class="modal-content">
       <div class="modal-header">
